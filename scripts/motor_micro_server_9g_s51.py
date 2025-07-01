8b84fb877e2bdc7767350bf889423126f14b1c95
canizalesjaime <canizales.jaime16@gmail.com>
2025-06-30 22:13:12 -0400

    arm motors to run

--- scripts/motor_micro_server_9g_s51.py
+++ scripts/motor_micro_server_9g_s51.py
@@ -1,38 +1,22 @@
-import lgpio
-import time
+from gpiozero import AngularServo
+from time import sleep 
 
-h = lgpio.gpiochip_open(0)
-SERVO_PIN = 18  # GPIO number
-FREQ = 50       # Servo expects 50 Hz
+servo1 = AngularServo(14, min_angle=0,max_angle=180,min_pulse_width=.5/1000,max_pulse_width=2.5/1000) #base
+servo2 = AngularServo(18, min_angle=0,max_angle=180,min_pulse_width=.5/1000,max_pulse_width=2.5/1000)
+servo3 = AngularServo(12, min_angle=0,max_angle=180,min_pulse_width=.5/1000,max_pulse_width=2.5/1000)
+servo4 = AngularServo(25, min_angle=0,max_angle=180,min_pulse_width=.5/1000,max_pulse_width=2.5/1000)
 
-def set_servo_pulse_us(pulse_width_us):
-    duty_percent = (pulse_width_us / 20000) * 100
-    print(f"Setting pulse width: {pulse_width_us} µs ({duty_percent:.2f}%)")
-    lgpio.tx_pwm(h, SERVO_PIN, FREQ, duty_percent)
 
 try:
     while True:
-        set_servo_pulse_us(1000)  # ~0°
-        time.sleep(1)
-        set_servo_pulse_us(2000)  # ~180°
-        time.sleep(1)
-
+        angle=int(input("Enter angle: (0 to 180):"))
+        servo1.angle=angle
+        sleep(1)
+        servo2.angle=angle
+        sleep(1)
+        servo3.angle=angle
+        sleep(1)
+        servo4.angle=angle
+        sleep(1)
 except KeyboardInterrupt:
-    print("Stopping")
-    lgpio.tx_pwm(h, SERVO_PIN, 0, 0)
-    lgpio.gpiochip_close(h)
-
-
-# import pigpio
-# import time
-
-# pi = pigpio.pi()
-
-# # Use GPIO18
-# SERVO_PIN = 13
-
-# while True:
-#     pi.set_servo_pulsewidth(SERVO_PIN, 1000)
-#     time.sleep(1)
-#     pi.set_servo_pulsewidth(SERVO_PIN, 2000)
-#     time.sleep(1)
+    print("stopped")
