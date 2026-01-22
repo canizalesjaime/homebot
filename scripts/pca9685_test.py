2f1fc1461c7d80a9c3db368fc5a1f6a66a0a9f26
canizalesjaime <canizales.jaime16@gmail.com>
2026-01-22 14:36:07 -0500

    added more arm prep and doc

--- scripts/pca9685_test.py
+++ scripts/pca9685_test.py
@@ -8,12 +8,6 @@ i2c = busio.I2C(board.SCL, board.SDA)
 pca = PCA9685(i2c)
 pca.frequency = 50
 
-# ------------------ PWM Calibration ------------------
-# Wide pulse range for near-180° travel
-# Adjust slightly per servo if needed
-MIN_PULSE = 150   # ~0.75 ms
-MAX_PULSE = 550   # ~2.7 ms
-
 # ------------------ Per-servo angle limits ------------------
 SERVO_LIMITS = {
     0: (10, 170),   # wrist roll (MG90S)
@@ -26,6 +20,16 @@ SERVO_LIMITS = {
 
 # ------------------ Servo Functions ------------------
 def set_servo_angle(channel, angle):
+    # PWM Calibration 
+    # Wide pulse range for near-180° travel
+    # Adjust slightly per servo if needed
+    # note: since 50HZ, one cycle last 20 ms
+    # this is # of ticks out of 4096 to represent high
+    # 4096 comes from the PCA9685’s 12-bit PWM resolution, 
+    # and 65535 comes from CircuitPython’s 16-bit duty-cycle interface.
+    # pulses below are estimated, and in general should be calibrated
+    MIN_PULSE = 150   # ~0.75 ms, 0 degrees
+    MAX_PULSE = 550   # ~2.7 ms, 180 degrees
     min_angle, max_angle = SERVO_LIMITS.get(channel, (0, 180))
     angle = max(min(angle, max_angle), min_angle)
 
@@ -33,7 +37,7 @@ def set_servo_angle(channel, angle):
     duty = int(pulse / 4096 * 65535)
     pca.channels[channel].duty_cycle = duty
 
-
+# ------------------ Servo Functions ------------------
 def move_smooth(channel, start, end, step=2, delay=0.02):
     if start < end:
         rng = range(start, end + 1, step)
@@ -43,44 +47,33 @@ def move_smooth(channel, start, end, step=2, delay=0.02):
     for angle in rng:
         set_servo_angle(channel, angle)
         time.sleep(delay)
+    time.sleep(2)
 
-
-def move_servo(channel):
+# ------------------ Servo Functions ------------------
+def move_servo_full_range(channel):
     min_angle, max_angle = SERVO_LIMITS.get(channel, (0, 180))
     move_smooth(channel, min_angle, max_angle)
-    time.sleep(0.5)
-    # move_smooth(channel, max_angle, min_angle)
-    # time.sleep(0.5)
+    time.sleep(2)
+
+# ------------------ Servo Functions ------------------
+def straighten_arm():
+    for s in SERVO_LIMITS: 
+        set_servo_angle(s,10)
+        time.sleep(2)     
+    move_smooth(1,10,70) 
+    move_smooth(7,10,170) 
+
+# ------------------ Servo Functions ------------------
+def pick_up_static_hacky():
+    move_smooth(3,10,130)
+    move_smooth(5,10,80)
+    move_smooth(0,10,80)
+    move_smooth(1,70,10)
 
 # ------------------ Run ------------------
 try:
-    # start pos (arm is straigthened out)
-    #USE MOVE SMOOTH INSTEAD
-    set_servo_angle(1,70) #gripper
-    time.sleep(2)
-    set_servo_angle(0,0) #wrist roll
-    time.sleep(2)
-    set_servo_angle(4,0) #elbow
-    time.sleep(2)
-    set_servo_angle(5,0) #shoulder
-    time.sleep(2)
-    set_servo_angle(3,0) #wrist pitch
-    time.sleep(2)
-    set_servo_angle(7,170) #base
-    time.sleep(2)
-
-    # grasp it- gripper
-    set_servo_angle(3,130)
-    time.sleep(2)
-    set_servo_angle(5,80)
-    time.sleep(2)     
-    set_servo_angle(4,0)
-    time.sleep(2)
-    set_servo_angle(0,30)
-    time.sleep(2)
-    set_servo_angle(1,0)
-    time.sleep(2)
-    pca.deinit()
+    straighten_arm()
+    pick_up_static_hacky()
 
 finally:
     pca.deinit()
