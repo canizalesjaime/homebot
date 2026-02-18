14c40b71561defefccdcbeea7152254d4550d25c
canizalesjaime <canizales.jaime16@gmail.com>
2026-02-17 23:19:41 -0500

    added lidar code

---
+++ scripts/lunar.py
@@ -0,0 +1,17 @@
+import serial
+
+# Open UART (Pi 4 default)
+ser = serial.Serial("/dev/serial0", 115200, timeout=1)
+
+print("Reading TF-Luna data...")
+
+while True:
+    if ser.in_waiting >= 9:
+        data = ser.read(9)
+        
+        # TF-Luna frame starts with 0x59 0x59
+        if data[0] == 0x59 and data[1] == 0x59:
+            distance = data[2] + data[3] * 256
+            strength = data[4] + data[5] * 256
+            
+            print(f"Distance: {distance} cm | Strength: {strength}")
