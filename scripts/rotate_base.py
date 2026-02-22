5772ce94849d338aad5a5df3e0881b31d2989f97
canizalesjaime <canizales.jaime16@gmail.com>
2026-02-21 23:54:34 -0500

    added rotate base test

---
+++ scripts/rotate_base.py
@@ -0,0 +1,61 @@
+import time
+import board
+import busio
+from adafruit_pca9685 import PCA9685
+
+
+class SmallArmNode():
+    def __init__(self):
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.pca = PCA9685(self.i2c)
+        self.pca.frequency = 50
+
+        self.SERVO_ANGLES = { 0: 90,   # base
+                              1: 90,   # elbow
+                              3: 90,   # shoulder
+                              4: 90,   # gripper
+        }
+
+        for serv in self.SERVO_ANGLES:
+            self.set_servo_angle(serv,90)
+
+
+    def set_servo_angle(self,channel, angle):
+        MIN_PULSE = 150   # ~0.75 ms, 0 degrees
+        MAX_PULSE = 550   # ~2.7 ms, 180 degrees
+        min_angle, max_angle = self.SERVO_LIMITS.get(channel, (0, 180))
+        angle = max(min(angle, max_angle), min_angle)
+
+        pulse = MIN_PULSE + (angle / 180.0) * (MAX_PULSE - MIN_PULSE)
+        duty = int(pulse / 4096 * 65535)
+        self.pca.channels[channel].duty_cycle = duty
+        self.SERVO_ANGLES[channel]=angle
+
+
+    def move_smooth(self,channel, start, end, step=2, delay=0.02):
+        if start < end:
+            rng = range(start, end + 1, step)
+        else:
+            rng = range(start, end - 1, -step)
+
+        for angle in rng:
+            self.set_servo_angle(channel, angle)
+            time.sleep(delay)
+
+
+    def rotate_base(self):
+        while True:
+            self.move_smooth(0,90,170)
+            self.move_smooth(0,170,90)
+            self.move_smooth(0,90,10)
+            self.move_smooth(0,10,90)
+
+
+
+def main():
+    try:
+        node =SmallArmNode()
+        node.rotate_base()
+
+    finally:
+        node.pca.deinit()
