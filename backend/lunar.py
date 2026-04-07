d49af18d9e1d70729265bceb13aa2c05c4661676
canizalesjaime <canizales.jaime16@gmail.com>
2026-04-06 23:52:23 -0400

    debugged lidar

--- backend/lunar.py
+++ backend/lunar.py
@@ -3,33 +3,28 @@ import serial
 
 class LunarNode():
     def __init__(self):
-        self.ser = serial.Serial("/dev/serial0", 115200, timeout=1)
+        self.ser = serial.Serial("/dev/ttyAMA0", 115200, timeout=1)
+        self.distance=-1
+        self.strength=-1
         print("Reading TF-Luna data...")
 
     def get_distance(self):
-        print(self.ser.in_waiting)
-        while self.ser.in_waiting >= 9:
+        #print(self.ser.in_waiting)
+        if self.ser.in_waiting >= 9:
             data = self.ser.read(9)
-            if self.ser.read(1)[0] ==0x59:
-                if self.ser.read(1)[0]==0x59:
-                    data=self.ser.read(7)
-                    distance=data[0]+data[1]*256
-                    return distance
             # TF-Luna frame starts with 0x59 0x59
-            #if data[0] == 0x59 and data[1] == 0x59:
-            #    distance = data[2] + data[3] * 256
-            #    strength = data[4] + data[5] * 256
-                
-            return None # in cm
+            if data[0] == 0x59 and data[1] == 0x59:
+               self.distance = data[2] + data[3] * 256
+               self.strength = data[4] + data[5] * 256
+        return (self.distance, self.strength) # in cm
 
 
 def main():
     node = LunarNode()
     try:
-        while(True):
-            d=node.get_distance()
-            if d!=None:
-                print(d)
+        while True:
+            d,s=node.get_distance()
+            print(f"Distance: {d} cm | Strength: {s}")
 
     except KeyboardInterrupt:
         print("\nExiting...")
