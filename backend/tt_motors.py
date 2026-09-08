f5532422680b1d2f9b0ae068a024590cc9202c1d
canizalesjaime <canizales.jaime16@gmail.com>
2026-09-08 16:36:55 -0400

    switched motors to only use lgpio

--- backend/tt_motors.py
+++ backend/tt_motors.py
@@ -3,51 +3,34 @@
 # The more time the the signal is high during a cycle the faster the speed.
 
 import time
-import gpiod
 import lgpio as GPIO
 
 
 class TtMotors():
-    TICKS_PER_WHEEL_REV = 1080 # 12 PPR × 90:1 gearbox
+    
 
     def __init__(self):
-        self.chip = gpiod.Chip('/dev/gpiochip0')
-        # gpios used in drivers_map
-        self.drivers_input_map ={"driverA_in1": self.chip.get_line(17), 
-                                 "driverA_in2": self.chip.get_line(27),
-                                 "driverB_in1": self.chip.get_line(23),
-                                 "driverB_in2": self.chip.get_line(24) }
-        self.drivers_enable_pin_map ={"driverA_enA":12,"driverB_enB":13}
-
-        self.stby = self.chip.get_line(25)
-        self.stby.request(consumer='motor_control',type=gpiod.LINE_REQ_DIR_OUT)
-        self.stby.set_value(1)
-
-        for name, line in self.drivers_input_map.items():
-            line.request(consumer='motor_control', type=gpiod.LINE_REQ_DIR_OUT)
-
         self.h = GPIO.gpiochip_open(0)
-        for name, pin in self.drivers_enable_pin_map.items():
-            GPIO.gpio_claim_output(self.h, pin) 
+        self.driver_map={"a_in1":17,"a_in2":27,"b_in1":23,"b_in2":24,
+                     "stby":25, "enA":12, "enB":13}
 
-        self.frequency =1000
+        for name, pin in self.driver_map.items():
+            GPIO.gpio_claim_output(self.h, pin)
+        GPIO.gpio_write(self.h, self.driver_map["stby"], 1)
+
+        self.encoder_pins={"left_a":5, "left_b":6, "right_a":16, "right_b":26}        
+        GPIO.gpio_claim_alert(self.h, self.encoder_pins["left_a"], GPIO.RISING_EDGE)
+        GPIO.gpio_claim_input(self.h, self.encoder_pins["left_b"])
+        GPIO.gpio_claim_alert(self.h, self.encoder_pins["right_a"], GPIO.RISING_EDGE)
+        GPIO.gpio_claim_input(self.h, self.encoder_pins["right_b"])
+        
+        #GPIO.gpio_set_debounce_micros(self.h, self.encoder_pins["left_a"], 1000)
+        #GPIO.gpio_set_debounce_micros(self.h, self.encoder_pins["right_a"], 1000)
+
+        self.ticks_per_wheel_rev = 1080 # 12 PPR × 90:1 gearbox
+        self.frequency=1000
         self.curr_speed=50
         self.set_speed(self.curr_speed)
-
-        # Encoder pins
-        self.left_encoder_a = 5
-        self.left_encoder_b = 6
-
-        self.right_encoder_a = 16
-        self.right_encoder_b = 26
-
-        GPIO.gpio_claim_alert(self.h, self.left_encoder_a, GPIO.RISING_EDGE)
-        GPIO.gpio_claim_input(self.h, self.left_encoder_b)
-        GPIO.gpio_claim_alert(self.h, self.right_encoder_a, GPIO.RISING_EDGE)
-        GPIO.gpio_claim_input(self.h, self.right_encoder_b)
-
-        GPIO.gpio_set_debounce_micros(self.h, self.left_encoder_a, 1000)
-        GPIO.gpio_set_debounce_micros(self.h, self.right_encoder_a, 1000)
 
         self.left_ticks = 0
         self.right_ticks = 0
@@ -61,14 +44,14 @@ class TtMotors():
         # Manufacturer's example uses rising edge of A. B determines direction.
         self.left_callback = GPIO.callback(
             self.h,
-            self.left_encoder_a,
+            self.encoder_pins["left_a"],
             GPIO.RISING_EDGE,
             self.left_encoder_event
         )
 
         self.right_callback = GPIO.callback(
             self.h,
-            self.right_encoder_a,
+            self.encoder_pins["right_a"],
             GPIO.RISING_EDGE,
             self.right_encoder_event
         )
@@ -98,35 +81,33 @@ class TtMotors():
         else:
             print("error wrong command")
 
-    # python version >= 3.7, and dictionaries are ordered by insert
+
     ###########################################################################
     def set_motor(self,motor_inputs):
-        i = 0 
-        for name, line in self.drivers_input_map.items():
-            line.set_value(motor_inputs[i])
-            i=i+1
+        GPIO.gpio_write(self.h, self.driver_map["a_in1"], motor_inputs[0])
+        GPIO.gpio_write(self.h, self.driver_map["a_in2"], motor_inputs[1])
+        GPIO.gpio_write(self.h, self.driver_map["b_in1"], motor_inputs[2])
+        GPIO.gpio_write(self.h, self.driver_map["b_in2"], motor_inputs[3])
         
 
     ###########################################################################
     def set_speed(self,percent):
         percent=min(max(percent,0),100)
-        
-        for name, pin in self.drivers_enable_pin_map.items():
-            GPIO.tx_pwm(self.h, pin, self.frequency, percent)
+        GPIO.tx_pwm(self.h, self.driver_map["enA"], self.frequency, percent)
+        GPIO.tx_pwm(self.h, self.driver_map["enB"], self.frequency, percent)
 
 
     # Encoder callbacks
     ###########################################################################
-
     def left_encoder_event(self, chip, gpio, level, timestamp):
-        if GPIO.gpio_read(self.h, self.left_encoder_b) == 0:
+        if GPIO.gpio_read(self.h, self.encoder_pins["left_b"]) == 0:
             self.left_ticks += 1
         else:
             self.left_ticks -= 1
 
     ################################################################
     def right_encoder_event(self, chip, gpio, level, timestamp):
-        if GPIO.gpio_read(self.h, self.right_encoder_b) == 0:
+        if GPIO.gpio_read(self.h, self.encoder_pins["right_b"]) == 0:
             self.right_ticks += 1
         else:
             self.right_ticks -= 1
@@ -135,10 +116,9 @@ class TtMotors():
     def get_ticks(self):
         return self.left_ticks, self.right_ticks
 
+
     ################################################################
-    # Calculate wheel angular velocity
-    ################################################################
-    def get_wheel_velocities(self):
+    def get_wheel_velocities(self): # angular
         current_time = time.monotonic()
 
         current_left_ticks = self.left_ticks
@@ -150,27 +130,18 @@ class TtMotors():
             return 0.0, 0.0
 
         # Number of ticks since last measurement
-        left_delta_ticks = (
-            current_left_ticks - self.previous_left_ticks
-        )
+        left_delta_ticks = current_left_ticks - self.previous_left_ticks
 
-        right_delta_ticks = (
-            current_right_ticks - self.previous_right_ticks
-        )
+        right_delta_ticks = current_right_ticks - self.previous_right_ticks
 
         # Save current values for next measurement
         self.previous_left_ticks = current_left_ticks
         self.previous_right_ticks = current_right_ticks
         self.previous_time = current_time
 
-        # ticks → revolutions
-        left_revolutions = (
-            left_delta_ticks / self.TICKS_PER_WHEEL_REV
-        )
-
-        right_revolutions = (
-            right_delta_ticks / self.TICKS_PER_WHEEL_REV
-        )
+        left_revolutions = left_delta_ticks / self.ticks_per_wheel_rev
+        right_revolutions = right_delta_ticks / self.ticks_per_wheel_rev
+        
 
         # revolutions → radians
         left_angle = left_revolutions * 2.0 * 3.14159265359
@@ -194,20 +165,17 @@ class TtMotors():
 
     ###########################################################################
     def release_lines(self):
-        for name, line in self.drivers_input_map.items():
-            line.set_value(0)
-            line.release()
-        
-        for name, pin in self.drivers_enable_pin_map.items():
-            GPIO.tx_pwm(self.h, pin, self.frequency, 0)
+        self.move("s")
+
+        GPIO.tx_pwm(self.h, self.driver_map["enA"], self.frequency, 0)
+        GPIO.tx_pwm(self.h, self.driver_map["enB"], self.frequency, 0)
+        GPIO.gpio_write(self.h, self.driver_map["stby"], 0)
 
         self.left_callback.cancel()
         self.right_callback.cancel()
 
         GPIO.gpiochip_close(self.h)
 
-        self.stby.set_value(0)
-        self.stby.release()
 
 ###############################################################################
 def main():
